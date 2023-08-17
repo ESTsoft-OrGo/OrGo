257e6403827342fc98382c44d5c3561bdf9d8885
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-08-17 15:22:26 +0900

    feat : Comment Write

--- post/views.py
+++ post/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Post, Comment
 
 # Create your views here.
+
+
+class CommentWrite(APIView):
+    def post(self, request):
+        user = request.user
+        post = Post.objects.get(id=request.data['post_id'])
+        comment = Comment.objects.create(writer=user,content=request.data['comment'],chat=post)
+        
+        datas = {
+            "message": "댓글 생성 완료",
+        }
+        return Response(datas,status=status.HTTP_201_CREATED)
