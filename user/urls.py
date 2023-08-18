30cb29e47f737c6f2c6b9d7720954be5ae49b8ce
bongsu <zio7542@naver.com>
2023-08-18 15:22:47 +0900

    Remove cached files

--- user/urls.py
+++ user/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from .views import Join, Login, Profile, Follow, Unfollow
+# from .views import Join, Login, Profile, Follow, Unfollow
+from .views import Follow
 
 app_name = 'user'
 
 urlpatterns = [
-    path("join/", Join.as_view(), name='join'),
-    path("login/", Login.as_view(), name="login"),
-    path("profile/", Profile.as_view(), name="profile"),
+    # path("join/", Join.as_view(), name='join'),
+    # path("login/", Login.as_view(), name="login"),
+    # path("profile/", Profile.as_view(), name="profile"),
     path("follow/", Follow.as_view(), name="follow"),
-    path("unfollow/", Unfollow.as_view(), name="unfollow"),
+    # path("unfollow/", Unfollow.as_view(), name="unfollow"),
 ]
 
--- user/views.py
+++ user/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework import status
+from user.models import User, Follower
+
 
 # Create your views here.
+class Follow(APIView):
+    
+    def post(self, request):
+        me = User.objects.get(email='test1@gmail.com')
+        
+        # 프로필 들어갔을 때나 게시글 작성자 user id
+        you = User.objects.get(email='test2@gmail.com')
+        
+        # 해당 프로필 user id하고 로그인한 id가 Follower에 들어가 있을 때 
+        following =  Follower.objects.filter(follower_id = me, target_id = you)
+
+        if not following:
+            follow = Follower.objects.create(follower_id= me, target_id=you, is_active=True)
+            
+            datas = {
+            "message":"follow"
+            }
+            
+            return Response(datas, status=status.HTTP_200_OK)
+        else:
+            unfollow = following.delete()
+            datas = {
+                "message":"unfollow"
+            }
+            return Response(datas, status=status.HTTP_200_OK)
