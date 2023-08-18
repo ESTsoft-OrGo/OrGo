d5a5151a8ac0345f305529d6f172730d7621f635
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-08-18 09:19:01 +0900

    feat : Add Test DB

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from .models import Profile, Follower
+
+User = get_user_model()
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Profile)
+admin.site.register(Follower)
