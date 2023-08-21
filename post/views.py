f69f52025718d8b7eddc2d75a0762c11cbde5577
su12 <ts04109@naver.com>
2023-08-21 11:49:11 +0900

    feat : post crud 기능 추가

--- post/views.py
+++ post/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework.response import Response
+from django.http import HttpResponse
+from .models import Post
+from .serializers import PostSerializer, Post_editSerializer
+from rest_framework import generics
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, DestroyAPIView
+from rest_framework.permissions import IsAuthenticated  
 
 # Create your views here.
+## Post
+
+class List(ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    
+
+class Write(CreateAPIView):
+    # permission_classes = [IsAuthenticated]
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class Edit(generics.RetrieveUpdateAPIView):
+    # permission_classes = [IsAuthenticated]
+    lookup_field = 'pk'
+    queryset = Post.objects.all()
+    serializer_class = Post_editSerializer
+
+
+class Delete(DestroyAPIView):
+    # permission_classes = [IsAuthenticated]
+    lookup_field = 'pk'
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    
+
+class View(RetrieveAPIView):
+    lookup_field = 'pk'
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
