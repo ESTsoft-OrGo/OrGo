62665008f0246c74e8b7ab1ba91b4c68e5fe2feb
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-08-24 14:49:51 +0900

    chore : Asgi Setting

--- Orgo/asgi.py
+++ Orgo/asgi.py
@@ -1,16 +1,27 @@
 """
-ASGI config for Orgo project.
-
+ASGI config for app project.
 It exposes the ASGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+import chat.routing
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Orgo.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+  "http": get_asgi_application(),
+  "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                chat.routing.websocket_urlpatterns
+            )
+        )
+    ),
+})
