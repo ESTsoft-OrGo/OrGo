4bd1b2e433060fd55b812b7de3c2ffb750814ff3
ra388 <ra993388@gmail.com>
2023-08-25 10:01:44 +0900

    chore : SQLite -> PostgreSQL 변경

--- Orgo/settings.py
+++ Orgo/settings.py
@@ -114,11 +114,23 @@ CORS_ORIGIN_ALLOW_ALL = True
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    "default": {
+        "ENGINE": os.environ.get("DB_ENGINE"),
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
+    }
+}
+'''
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
--- study/models.py
+++ study/models.py
@@ -3,36 +3,36 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-
 class Study(models.Model):
     leader = models.ForeignKey(User, on_delete=models.CASCADE, related_name='study_leader')
-    title = models.CharField(max_length=200,null=True,blank=True)
-    description = models.CharField(max_length=200,null=True,blank=True)
-    start_date = models.DateField()
-    end_date = models.DateField()
+    title = models.CharField(max_length=200, null=True, blank=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
     
     ON_OFF_CHOICES = [
-        ('ON', 'online'), 
-        ('OFF', 'offline'),
-        ]
+        ('ON', 'Online'), 
+        ('OFF', 'Offline'),
+    ]
     online_offline = models.CharField(max_length=5, choices=ON_OFF_CHOICES)
 
-    location = models.CharField(max_length=200,null=True)
-    max_participants = models.IntegerField(default=0)
+    location = models.CharField(max_length=200, null=True)
+    max_participants = models.PositiveIntegerField(default=0)
     
     STATUS_CHOICES = [
-        ('종료', '종료'),
-        ('모집중', '모집중'),
-        ('진행중','진행중'),
+        ('END', 'End'),
+        ('RECRUITING', 'Recruiting'),
+        ('ONGOING', 'Ongoing'),
     ]
-    status = models.CharField(max_length=5, choices=STATUS_CHOICES)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
     
-    participants = models.ManyToManyField(User, related_name='study_participants', blank=True)
     is_active = models.BooleanField(default=True)
+    participants = models.ManyToManyField(User, related_name='study_participants', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
-class Tags(models.Model):
-    Study = models.ForeignKey(Study, on_delete=models.CASCADE)
+class Tag(models.Model):
+    study = models.ForeignKey(Study, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
+
