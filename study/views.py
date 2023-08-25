6190baeb75d29af6ee662f2fb83be0f7845392e1
ra388 <ra993388@gmail.com>
2023-08-25 13:59:29 +0900

    fix : study join/cancel에 IsAuthenticated 추가

--- study/views.py
+++ study/views.py
@@ -1,9 +1,10 @@
 from django.db.models import Q
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
-from .models import Study
 from django.shortcuts import get_object_or_404
+from .models import Study
 from .serializers import StudySerializer
 
 class StudySearch(APIView):
@@ -15,15 +16,17 @@ class StudySearch(APIView):
         serializer = StudySerializer(studies, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-
 class StudyJoin(APIView):
+    permission_classes = [IsAuthenticated]
+
     def post(self, request):
         study_id = request.data.get('study_id')
         study = get_object_or_404(Study, id=study_id)
-        
+        user = request.user
+
         if study.participants.count() < study.max_participants:
-            if request.user not in study.participants.all():
-                study.participants.add(request.user)
+            if user not in study.participants.all():
+                study.participants.add(user)
                 return Response({"message": "스터디 참가가 완료되었습니다."}, status=status.HTTP_201_CREATED)
             else:
                 return Response({"message": "이미 참가한 스터디입니다."}, status=status.HTTP_400_BAD_REQUEST)
@@ -31,12 +34,15 @@ class StudyJoin(APIView):
             return Response({"message": "참가 인원이 마감되었습니다."}, status=status.HTTP_400_BAD_REQUEST)
 
 class StudyCancel(APIView):
+    permission_classes = [IsAuthenticated]
+
     def post(self, request):
         study_id = request.data.get('study_id')
         study = get_object_or_404(Study, id=study_id)
-        
-        if request.user in study.participants.all():
-            study.participants.remove(request.user)
+        user = request.user
+
+        if user in study.participants.all():
+            study.participants.remove(user)
             return Response({"message": "스터디 참가가 취소되었습니다."}, status=status.HTTP_200_OK)
         else:
             return Response({"message": "참가하지 않은 스터디입니다."}, status=status.HTTP_400_BAD_REQUEST)
