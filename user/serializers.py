b9dde12872729ae37c25a584203675d8592b2690
kim-ido <kimido987@gmail.com>
2023-08-28 10:30:29 +0900

    fix(user): Fix join validation issues
    회원가입시 이메일 형식 이슈 해결, 비밀번호 확인 로직 추가
    
    close #19

--- user/serializers.py
+++ user/serializers.py
@@ -5,8 +5,9 @@ from .models import User, Profile
 
 
 class UserSerializer(serializers.ModelSerializer):
-    email = serializers.CharField(max_length=80)
+    email = serializers.EmailField(max_length=80)
     password = serializers.CharField(min_length=8, write_only=True)
+    password_confirm = serializers.CharField(write_only=True)
     profileImage = serializers.ImageField(source='profile.profileImage', read_only=True)
     nickname = serializers.CharField(source='profile.nickname', read_only=True)
     about = serializers.CharField(source='profile.about', read_only=True)
@@ -14,18 +15,24 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['email', 'password', 'profileImage', 'nickname', 'about', 'id']
+        fields = ['email', 'password', 'password_confirm', 'profileImage', 'nickname', 'about', 'id']
 
     def validate(self, attrs):
-        email_exists=User.objects.filter(email=attrs['email']).exists()
+        email = attrs.get('email')
+        password = attrs.get('password')
+        password_confirm = attrs.get('password_confirm')
 
-        if email_exists:
+        if User.objects.filter(email=email).exists():
             raise ValidationError("중복된 이메일입니다.")
-        
-        return super().validate(attrs)
+
+        if password != password_confirm:
+            raise ValidationError("비밀번호와 비밀번호 확인이 일치하지 않습니다.")
+
+        return attrs
     
     def create(self, validated_data):
         password = validated_data.pop("password")
+        validated_data.pop("password_confirm")
 
         user = super().create(validated_data)
         user.set_password(password)
