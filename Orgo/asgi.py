2c7bda911799167c5ca1afb7759b571a5667dcaa
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-09-01 11:00:23 +0900

    chore : Asgi 세팅 변경

--- Orgo/asgi.py
+++ Orgo/asgi.py
@@ -6,14 +6,16 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Orgo.settings')
+django_asgi_app = get_asgi_application()
+
 import chat.routing
 import notify.routing
 from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Orgo.settings')
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
