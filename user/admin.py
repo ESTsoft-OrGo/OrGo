f4a32f90db4238c65573d9153885bde504142304
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-09-15 19:07:44 +0900

    feat : 채팅 서버와 모델 동기화

--- user/admin.py
+++ user/admin.py
@@ -1,17 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
-from .models import Profile, Follower
+from .models import Profile, Follower, Blacklist
 
 User = get_user_model()
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'is_active', 'is_staff', 'is_superuser', 'liked_post')
+    list_display = ('email', 'is_active', 'is_staff',
+                    'is_superuser', 'liked_post')
     list_filter = ('is_active', 'is_staff', 'is_superuser')
     search_fields = ('email',)
-    
+
     def liked_post(self, obj):
-        liked_posts=obj.liked_posts.all()
+        liked_posts = obj.liked_posts.all()
         posts = []
         for post in liked_posts.all():
             posts.append(post.title)
@@ -22,3 +23,4 @@ class UserAdmin(admin.ModelAdmin):
 admin.site.register(User, UserAdmin)
 admin.site.register(Profile)
 admin.site.register(Follower)
+admin.site.register(Blacklist)
