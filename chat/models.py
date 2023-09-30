97599398edcdab64e48e5fb5750eb76a4bb2ea12
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-09-30 20:23:22 +0900

    feat : 그룹채팅 모델 설계 및 코드 위치 변경

--- chat/models.py
+++ chat/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from notify.models import Notification
 from django.db.models.signals import post_save
+from study.models import Study
 
 User = get_user_model()
 
@@ -9,8 +10,8 @@ User = get_user_model()
 class Room(models.Model):
     title = models.CharField(max_length=200,null=True,blank=True,unique=True)
     is_active = models.BooleanField(default=True)
-    firstuser = models.ForeignKey(User, on_delete=models.CASCADE ,related_name="firstuser")
-    seconduser = models.ForeignKey(User, on_delete=models.CASCADE,related_name="seconduser")
+    firstuser = models.ForeignKey(User, on_delete=models.CASCADE ,related_name="firstuser",null=True)
+    seconduser = models.ForeignKey(User, on_delete=models.CASCADE,related_name="seconduser",null=True)
     joinUser = models.IntegerField(default=0,null=True,blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -25,31 +26,8 @@ post_save.connect(room_action, sender=Room)
 
 class Message(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
-    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
     is_read = models.BooleanField(default=False)
     content = models.CharField(max_length=200,null=True,blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
-
-class GroupChat(models.Model):
-    title = models.CharField(max_length=200, null=True, blank=True, unique=True)
-    is_active = models.BooleanField(default=True)
-    leader = models.ForeignKey(User, on_delete=models.CASCADE, related_name='leader')
-    members = models.ManyToManyField(User, related_name="group_chats")
-
-# def group_chat_action(sender, **kwargs):
-#     if kwargs['created']:
-#         group_chat = kwargs['instance']
-#         content = f'단체 채팅방을 생성하였습니다.'
-        
-#         for member in group_chat.members.all():
-#             Notification.objects.create(sender=group_chat.leader, receiver=member, content=content)
-
-# post_save.connect(group_chat_action, sender=GroupChat)
-
-
-class GroupMessage(models.Model):
-    chat = models.ForeignKey(GroupChat, on_delete=models.CASCADE)
-    writer = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.CharField(max_length=200, null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
--- study/models.py
+++ study/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
 
 User = get_user_model()
 
@@ -30,6 +31,40 @@ class Study(models.Model):
     participants = models.ManyToManyField(User, related_name='study_participants', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+def on_post_save_for_study(sender, **kwargs):
+    if kwargs['created']:
+        study = kwargs['instance']
+        groupChat = GroupChat.objects.create(study=study)
+        groupChat.title = f'studyroom{groupChat.pk}'
+        groupChat.save()
+
+post_save.connect(on_post_save_for_study, sender=Study)
+
+class GroupChat(models.Model):
+    title = models.CharField(max_length=200, null=True, blank=True, unique=True)
+    is_active = models.BooleanField(default=True)
+    leader = models.ForeignKey(User, on_delete=models.CASCADE, related_name='leader',null=True)
+    study = models.OneToOneField(Study,on_delete=models.CASCADE,null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # members = models.ManyToManyField(User, related_name="group_chats")
+
+# def group_chat_action(sender, **kwargs):
+#     if kwargs['created']:
+#         group_chat = kwargs['instance']
+#         content = f'단체 채팅방을 생성하였습니다.'
+        
+#         for member in group_chat.members.all():
+#             Notification.objects.create(sender=group_chat.leader, receiver=member, content=content)
+
+# post_save.connect(group_chat_action, sender=GroupChat)
+
+
+class GroupMessage(models.Model):
+    chat = models.ForeignKey(GroupChat, on_delete=models.CASCADE)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Tag(models.Model):
