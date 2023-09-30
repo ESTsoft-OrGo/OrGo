3537fe8ea816c33158263a86b0e1b551f219927c
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-09-30 20:23:40 +0900

    fix : 그룹채팅 시리얼라이저 삭제

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,7 +1,1 @@
 from rest_framework import serializers
-from .models import GroupChat
-
-class GroupChatSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = GroupChat
-        fields = '__all__'
