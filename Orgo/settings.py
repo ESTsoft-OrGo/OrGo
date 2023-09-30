404c17937b94546cffa846aa239a953c594278aa
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-09-30 20:24:36 +0900

    feat : 그룹채팅방 Admin 페이지 등록

--- Orgo/settings.py
+++ Orgo/settings.py
@@ -29,6 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+SITE_ID = 1
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
--- study/admin.py
+++ study/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Study, Tag
+from .models import Study, Tag, GroupChat, GroupMessage
 # Register your models here.
 
 admin.site.register(Study)
 admin.site.register(Tag)
+admin.site.register(GroupChat)
+admin.site.register(GroupMessage)
