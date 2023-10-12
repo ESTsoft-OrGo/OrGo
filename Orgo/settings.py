72531a3f69778803a4aa7f808748e4590c71d301
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-10-12 19:25:11 +0900

    fix : 환경변수 추가

--- Orgo/settings.py
+++ Orgo/settings.py
@@ -96,7 +96,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = 'vpxpxtizprobqegy'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 ROOT_URLCONF = 'Orgo.urls'
 
